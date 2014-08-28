770eff6b43ca5004c53219667f8e8159adbbd6d0
Kyle Girard <kyle@kdmanalytics.com>
2014-08-28 18:05:37 -0400

    Add tests for enabled functions within Degree class

--- test/cagey/math/DegreeTest.cc
+++ test/cagey/math/DegreeTest.cc
@@ -1,6 +1,6 @@
+
 #include <cagey/math/Degree.hh>
 #include <gtest/gtest.h>
-
 
 using namespace cagey::math;
 
@@ -16,6 +16,19 @@ TEST(Degree, ValueConstructor) {
   EXPECT_FLOAT_EQ(45.0f, float(d));
 }
 
+TEST(Degree, CoversionCopyConstructor) {
+  Degree<int> d{45};
+  Degree<float> d2{d};
+
+  EXPECT_FLOAT_EQ(45.0f, float(d2));
+}
+
+TEST(Degree, CopyConstructor) {
+  Degree<float> d{45};
+  Degree<float> d2{d};
+
+  EXPECT_FLOAT_EQ(45.0f, float(d2));
+}
 
 TEST(Degree, AddAssignOperator) {
   Degree<float> d{45.0f};
@@ -25,6 +38,21 @@ TEST(Degree, AddAssignOperator) {
   EXPECT_FLOAT_EQ(90.0f, float(d));
 }
 
+TEST(Degree, SubtractAssignOperator) {
+  Degree<float> d{45.0f};
+  Degree<float> d2{45.0f};
+  d -= d2;
+
+  EXPECT_FLOAT_EQ(0.0f, float(d));
+}
+
+TEST(Degree, MultiplicationAssignOperator) {
+  Degree<float> d{45.0f};
+  d *= 3.0f;
+
+  EXPECT_FLOAT_EQ(135.0f, float(d));
+}
+
 TEST(Degree, AddOperator) {
   Degree<float> d{45.0f};
   Degree<float> d2{45.0f};
@@ -32,3 +60,28 @@ TEST(Degree, AddOperator) {
 
   EXPECT_FLOAT_EQ(90.0f, float(d3));
 }
+
+TEST(Degree, SubtractOperator) {
+  Degree<float> d{45.0f};
+  Degree<float> d2{45.0f};
+  auto d3 = d - d2;
+
+  EXPECT_FLOAT_EQ(0.0f, float(d3));
+}
+
+TEST(Degree, MultiplicationOperator) {
+  Degree<float> d{45.0f};
+  auto d2 = d * 3.0f;
+  auto d3 = 2.0f * d;
+
+  EXPECT_FLOAT_EQ(135.0f, float(d2));
+  EXPECT_FLOAT_EQ(90.0f, float(d3));
+}
+
+TEST(Degree, equals) {
+  Degree<float> d{45.0f};
+  Degree<float> d2{45.0f};
+  EXPECT_TRUE(equals(d,d2));
+  EXPECT_FALSE(equals(d,Degree<float>{30}));
+}
+
